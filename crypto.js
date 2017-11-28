cc10543761ac4ea92a39423215c79d12e4975785
olic0 <olivier.couque@live.fr>
2017-11-28 13:14:18 +0100

    Working on brushin

--- crypto.js
+++ crypto.js
@@ -36,7 +36,6 @@ var brush = d3.brushX()
     .extent([[0, 0], [width, height2]])
     .on("brush end", brushed);
 
-
 var focus = svg.append("g") //Focus is the svg for the graph
     .attr("class", "focus")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
@@ -45,89 +44,96 @@ var context = svg.append("g") //Context is the svg for the brush
     .attr("class", "context")
     .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
 
-d3.csv("cleaning/crypto_prices.csv", function(error, data) {
-  if (error) throw error;
-  //Give values to axis
-  //x.domain(data.map(function(d) { return d.Date; }));
-  //console.log(d3.extent(data, function(d) {return d.value; }));
-  //y.domain(d3.extent(data, function(d) {return d.value; })).nice();
-  var day_prices = {};
+//Read csv file only once and keep the day prices of all currencies in a JS object
+var day_prices = {};
+//List to keep name of currencies
+var currencies = [];
+
+//Function that fill the day_prices object with the csv data
+function fillPrices(data){
   data.forEach(function(d){
     day_prices[d.Date] = [d.BTC, d.ETH, d.NEO, d.XRP, d.BCH, d.DASH, d.BCC, d.ETC, d.MIOTA, d.LTC, d.XMR, d.XEM, d.NMR, d.STRAT, d.WAVES];
   });
+}
 
-  let currencies = [];
-  let percentages = [];
-  // To change with brushing
-  let beginDate = "Sep 01 2017";
-  let endDate = "Nov 05 2017";
-  let oldPrice = day_prices[beginDate];
-  let newPrice = day_prices[endDate];
-  console.log(day_prices[beginDate]);
-  console.log(day_prices[endDate]);
-  //let colBTC = data.map(function(d) { return d.BTC });
-  //let colETH = data.map(function(d) { return d.ETH });
-  //let colNEO = data.map(function(d) { return d.NEO });
-
-  // Get currencies name
-  for (let prop in data[0]) {
-    if (data[0].hasOwnProperty(prop)) {
+//Function that fill the currencies list names given the corresponding row of the csv file
+function fillCurrencies(row){
+  for (let prop in row) {
+    if (row.hasOwnProperty(prop)) {
       if (prop != 'Date') {
         currencies.push(prop);
       }
     }
   };
+}
 
-  let currenciesCount = currencies.length;
-  for(let i = 0; i < currenciesCount; i++) {
-    let percentage = getPercentage(oldPrice[i], newPrice[i]);
-    percentages.push(percentage);
-  }
-
-  let maxPercentage = d3.max(percentages);
-  let minPercentage = d3.min(percentages);
-
+d3.csv("cleaning/crypto_prices.csv", function(error, data) {
+  if (error) throw error;
+  fillPrices(data);
+  fillCurrencies(data[0]);
+  updateGraph("Apr 28 2013", "Nov 07 2017");
   x.domain(currencies);
-  y.domain([minPercentage, maxPercentage]);
-  x2.domain([parseDate("Apr 2013"), parseDate("Nov 2017")]);
-
-  console.log(percentages)
-
-  focus.selectAll(".bar") //Compute, place and draw every bar
-      .data(percentages)
-    .enter().append("rect")
-      .attr("class", function(d) { return "bar bar--" + (d < 0 ? "negative" : "positive"); })
-      .attr("y", function(d) { return y(Math.max(0, d)); })
-      .attr("x", function(d, i) { return x(currencies[i]); })
-      .attr("height", function(d) { return Math.abs(y(d) - y(0)); })
-      .attr("width", x.bandwidth());
-
-  focus.append("g") //Append x axis of graph
-      .attr("class", "axis axis--x")
-      .attr("transform", "translate(0,"+ y(0) + ")") //height/2 + margin.top
-      .call(xAxis);
-
-  focus.append("g") //Append y axis of graph
-      .attr("class", "axis axis--y")
-      .call(yAxis);
-
-  context.append("g") //Axis for brush
-      .attr("class", "axis axis--x")
-      .attr("transform", "translate(0," + height2/2 + ")")
-      .call(xAxis2);
-
-  context.append("g") //Selecting area on brush
-      .attr("class", "brush")
-      .call(brush)
-      .call(brush.move, x.range());
-
 });
+
+x2.domain([parseDate("Apr 2013"), parseDate("Nov 2017")]);
+
+//Function that updates the graph when brushing
+function updateGraph(beginDate, endDate){
+
+    let percentages = [];
+    let oldPrice = day_prices[beginDate];
+    let newPrice = day_prices[endDate];
+    console.log(oldPrice);
+    let currenciesCount = currencies.length;
+    for(let i = 0; i < currenciesCount; i++) {
+      let percentage = getPercentage(oldPrice[i], newPrice[i]);
+      percentages.push(percentage);
+    }
+
+    let maxPercentage = d3.max(percentages);
+    let minPercentage = d3.min(percentages);
+
+    y.domain([minPercentage, maxPercentage]);
+
+    focus.selectAll(".bar") //Compute, place and draw every bar
+        .data(percentages)
+      .enter().append("rect")
+        .attr("class", function(d) { return "bar bar--" + (d < 0 ? "negative" : "positive"); })
+        .attr("y", function(d) { return y(Math.max(0, d)); })
+        .attr("x", function(d, i) { return x(currencies[i]); })
+        .attr("height", function(d) { return Math.abs(y(d) - y(0)); })
+        .attr("width", x.bandwidth());
+}
+
+focus.append("g") //Append x axis of graph
+    .attr("class", "axis axis--x")
+    .attr("transform", "translate(0,"+ y(0) + ")") //height/2 + margin.top
+    .call(xAxis);
+
+focus.append("g") //Append y axis of graph
+    .attr("class", "axis axis--y")
+    .call(yAxis);
+
+context.append("g") //Axis for brush
+    .attr("class", "axis axis--x")
+    .attr("transform", "translate(0," + height2/2 + ")")
+    .call(xAxis2);
+
+context.append("g") //Selecting area on brush
+    .attr("class", "brush")
+    .call(brush)
+    .call(brush.move, x.range());
 
 function brushed() {
   var s = d3.event.selection || x2.range();
   var time_interval = s.map(x2.invert, x2);
   //Display somewhere the exact time interval
   //Compute new values for each currency and display new bars
+  var split_begin = time_interval[0].toString().split(" ");
+  var split_end = time_interval[1].toString().split(" ");
+  var begin_date = split_begin[1] + " " + split_begin[2] + " " + split_begin[3];
+  var end_date = split_end[1] + " " + split_end[2] + " " + split_end[3];
+  updateGraph(begin_date, end_date);
 }
 
 function type(d) { //Function that parses the data on read
